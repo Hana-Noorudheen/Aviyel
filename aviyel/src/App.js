36931634ee587b529f51749767eccc147992e0c8
Devaprasad <devaprasadmohan@gmail.com>
2022-03-18 20:59:15 +0530

    Updated Card.js

--- aviyel/src/App.js
+++ aviyel/src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div>
       <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
--- aviyel/src/components/cards.js
+++ aviyel/src/components/cards.js
@@ -6,10 +6,16 @@ const Card = () => {
                 <div className="card-image">
                     <img src="https://picsum.photos/200/300" alt=""/>          
                 </div> 
-                <div className="card-content">
+                <div  className="card-content">
                     <h2>Vaishnav Agarwal</h2>
                     <h4>Ux/Ui Designer</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                    <div className="card-button">
+                        <button>Contribution link</button>
+                        <button>Social Connect</button>
+
+                        
+                    </div>
                 </div>
             </div>
         </>
