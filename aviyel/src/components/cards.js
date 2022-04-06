2735cb8aea65d18a9ace84636af0c4e985d4d6e5
noorhana123 <hananoorudheen721@gmail.com>
2022-04-07 00:59:45 +0530

    Flags,socialmediaurl,contributionsUrl

--- aviyel/src/components/cards.js
+++ aviyel/src/components/cards.js
@@ -2,17 +2,21 @@ import "./cards.css";
 import { Data } from "./data";
 import b1 from "../assets/images/b1.svg";
 import b2 from "../assets/images/b2.svg";
+import { findFlagUrlByNationality } from "country-flags-svg";
+
 const CardCollection = () => {
   return (
     <>
       <div className="parent">
         {Data.map((item) => (
           <Card
-            image={item.pic}
+            image={item.profilePhotoURL}
             title={item.title}
             name={item.Name}
             desc={item.desc}
-            flag={item.flag}
+            flag= {findFlagUrlByNationality(item.country)}
+            socialMediaURL={item.socialMediaURL}
+            contributionsURL={item.contributionsURL}
           />
         ))}
       </div>
@@ -20,7 +24,7 @@ const CardCollection = () => {
   );
 };
 
-const Card = ({ image, title, name, desc, flag }) => {
+const Card = ({ image, title, name, desc, flag,socialMediaURL,contributionsURL }) => {
   return (
     <>
       <div className="card">
@@ -36,14 +40,14 @@ const Card = ({ image, title, name, desc, flag }) => {
           <div className="card-button">
             <span className="b1">
               <img src={b1} alt="" />
-              <a link href="#">
+              <a link href={contributionsURL}>
                 Contribution
               </a>
             </span>
 
             <span className="b1">
               <img src={b2} alt="" />
-              <a href="#">Social Connect</a>
+              <a href={socialMediaURL}>Social Connect</a>
             </span>
           </div>
         </div>
--- aviyel/src/components/data.js
+++ aviyel/src/components/data.js
@@ -1,40 +1,49 @@
-import { findFlagUrlByNationality } from "country-flags-svg";
-import vaibhav from "../assets/images/vaibhav.jpeg";
-import dp from "../assets/images/dp.jpg";
-import revanth from "../assets/images/revanth.jpg";
-import hana from "../assets/images/hana.jpeg";
+// import { findFlagUrlByNationality } from "country-countrys-svg";
+// import vaibhav from "../assets/images/vaibhav.jpeg";
+// import dp from "../assets/images/dp.jpg";
+// import revanth from "../assets/images/revanth.jpg";
+// import hana from "../assets/images/hana.jpeg";
 
-const flagUrl = findFlagUrlByNationality("Indian");
+// const "Indian" = findFlagUrlByNationality("Indian");
+
 export const Data = [
   {
-    pic: vaibhav,
-    flag: flagUrl,
+    profilePhotoURL: "https://picsum.photos/200/300",
+    country: "Nigerian",
     Name: "Vaibhav Agarwal",
     title: "Full Stack Developer",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    socialMediaURL: "https://www.facebook.com/",
+    contributionsURL: "https://github.com"
   },
 
   {
-    pic: dp,
-    flag: flagUrl,
+    profilePhotoURL: "https://picsum.photos/200/300",
+    country: "Indian",
     Name: "Devaprasad NM",
     title: "Btech Student",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    socialMediaURL: "https://www.facebook.com/",
+    contributionsURL: "https://github.com"
   },
 
   {
-    pic: revanth,
-    flag: flagUrl,
+    profilePhotoURL: "https://picsum.photos/200/300",
+    country: "Indian",
     Name: "Revanth",
     title: "Btech Student",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elittt.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    socialMediaURL: "https://www.facebook.com/",
+    contributionsURL: "https://github.com"
   },
   {
-    pic: hana,
-    flag: flagUrl,
+    profilePhotoURL: "https://picsum.photos/200/300",
+    country: "Indian",
     Name: "Hana Noorudheen",
     title: "Btech Student",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    socialMediaURL: "https://www.facebook.com/",
+    contributionsURL: "https://github.com"
   },
 
 ];
